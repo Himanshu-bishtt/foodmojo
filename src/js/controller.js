aa610843c94539105d861a6d6607648b13bdb537
Himanshu Bisht <himanshubisht313@gmail.com>
2022-04-14 17:05:58 +0530

    hero heading animation on load

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,8 @@
 'use strict';
+
+window.addEventListener('load', function () {
+  const heroHeading = this.document.querySelector('.hero__content--heading');
+
+  heroHeading.style.filter = 'blur(0)';
+  heroHeading.style.letterSpacing = '0';
+});
