75af1e24aa02175b9f28c1471e541940c55315e3
Himanshu Bisht <himanshubisht313@gmail.com>
2022-04-16 19:35:47 +0530

    recipes image changed from dynamic to static, popup close timer increased

--- src/js/config.js
+++ src/js/config.js
@@ -3,4 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const FORKIFY_API_KEY = '<YOUR_KEY>';
 export const GEOCODE_API_KEY = '784130905068214839321x63695';
-export const MODAL_CLOSE_SEC = 3.5;
+export const MODAL_CLOSE_SEC = 5;
