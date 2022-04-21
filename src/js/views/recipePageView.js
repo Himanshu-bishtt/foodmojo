4d1f7247218ebff73e16bccd344c9aa6697f9cd7
Himanshu Bisht <himanshubisht313@gmail.com>
2022-04-21 20:21:34 +0530

    recipe page dynamically loaded with image and title

--- src/js/views/recipePageView.js
+++ src/js/views/recipePageView.js
@@ -1,5 +1,5 @@
 class RecipePageView {
-  #parentElement = document.querySelector('.recipe');
+  #parentElement = document.querySelector('.recipe__content');
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
@@ -9,11 +9,16 @@ class RecipePageView {
     this.#parentElement.innerHTML = '';
 
     const html = `
-    <p class="recipe__title">${recipe.title}</p>
-    <p class="recipe__publisher">${recipe.publisher}</p>
-    <p class="recipe__servings">${recipe.servings}</p>
-    <p class="recipe__time">${recipe.cooking_time}</p>
-    <p>TEST</p>
+      <figure class="recipe__fig">
+        <img
+          src="${recipe.image_url}"
+          alt="${recipe.title}"
+          class="recipe__img"
+        />
+        <h1 class="recipe__title">
+          <span>${recipe.title}</span>
+        </h1>
+      </figure>
     `;
 
     this.#parentElement.insertAdjacentHTML('beforeend', html);
