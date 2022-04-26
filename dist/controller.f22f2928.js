585f4e776488035a61fd3a4a4fecfc264ac0b568
Himanshu Bisht <himanshubisht313@gmail.com>
2022-04-26 20:18:33 +0530

    chore: implementing versioning in project foodmojo

--- dist/controller.f22f2928.js
+++ dist/controller.f22f2928.js
@@ -17049,7 +17049,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40347" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38561" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
